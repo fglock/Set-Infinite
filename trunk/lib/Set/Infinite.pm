ca4fe99de4c31786a63d0c0516115e3b192d2299
fglock <fglock@49043108-e40d-0410-ab17-85caa8b5b18d>
2003-09-12 22:42:06 +0000

    pod - copyright
    git-svn-id: https://perl-date-time.svn.sourceforge.net/svnroot/perl-date-time/modules/Set-Infinite@3207 49043108-e40d-0410-ab17-85caa8b5b18d

--- trunk/lib/Set/Infinite.pm
+++ trunk/lib/Set/Infinite.pm
@@ -1877,5 +1877,14 @@ The perl-date-time project <http://datetime.perl.org>
 
 Flavio Soibelmann Glock <fglock@pucrs.br>
 
+=head1 COPYRIGHT
+
+Copyright (c) 2003 Flavio Soibelmann Glock.  All rights reserved.  
+This program is free software; you can redistribute it and/or modify 
+it under the same terms as Perl itself.
+
+The full text of the license can be found in the LICENSE file included
+with this module.
+
 =cut
 
